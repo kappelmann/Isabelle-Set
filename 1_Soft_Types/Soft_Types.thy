cccb810f8461c22d76e63c2d37a45cdbc7360883
Josh Chen <jaycech3n@gmail.com>
2019-04-11 21:06:58 +0200

    Just formatting

--- 1_Soft_Types/Soft_Types.thy
+++ 1_Soft_Types/Soft_Types.thy
@@ -14,7 +14,8 @@ subsection \<open> Notion of soft type \<close>
 
 (* Just an embedding of Pure predicates into a new type, for more explicit handling.
 Note the type argument: We can assign a soft type to any object in Pure, including
-meta-level functions. *)
+meta-level functions.
+*)
 typedecl 'a type
 
 axiomatization 
@@ -29,9 +30,7 @@ lemma has_type_Type[simp]: "x ::: Type P \<equiv> P x"
   unfolding has_type_def pred_of_Type .
 
 
-
 subsection \<open> Pi-Types \<close>
-
 
 definition Pi_type :: "'a type \<Rightarrow> ('a \<Rightarrow> 'b type) \<Rightarrow> ('a \<Rightarrow> 'b) type"
   where
@@ -42,15 +41,13 @@ syntax
 translations
   "(x : A) \<Rightarrow> B" \<rightleftharpoons> "CONST Pi_type A (\<lambda>x. B)"
 
-term "(x : A) \<Rightarrow> (y : B) \<Rightarrow> C y"
-
 
 lemma Pi_typeI:
-  assumes a: "\<And>x. x ::: A \<Longrightarrow> f x ::: B x"
+  assumes "\<And>x. x ::: A \<Longrightarrow> f x ::: B x"
   shows "f ::: (x:A) \<Rightarrow> B x"
   unfolding Pi_type_def has_type_Type
-  by (rule a)
-  
+  by fact
+
 lemma Pi_typeE:
   assumes 1: "f ::: (x: A) \<Rightarrow> B x"
   assumes 2: "x ::: A"
@@ -58,7 +55,7 @@ lemma Pi_typeE:
 proof -
   from 2
   show "PROP ?thesis"
-    by (rule 1[unfolded Pi_type_def has_type_Type, rule_format])
+  by (rule 1[unfolded Pi_type_def has_type_Type, rule_format])
 qed
 
 
@@ -83,7 +80,6 @@ lemma Int_TypeD2:
   by (rule conjunctionD2)
 
 
-
 subsection \<open> Some Tooling \<close>
 
 ML_file "soft_type.ML"
@@ -92,7 +88,6 @@ ML_file "soft_type_inference.ML"
 
 
 (* See Old_ZF_Examples/ZF_Typing_Examples.thy for an example with type inference *)
-
 
 
 end
