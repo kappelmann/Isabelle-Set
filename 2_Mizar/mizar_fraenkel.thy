f68de62fd0623f82c9a6fa65e7c624c6a40f5f12
Josh Chen <jaycech3n@gmail.com>
2019-04-17 17:20:39 +0200

    finished formatting mizar_fraenkel

--- 2_Mizar/mizar_fraenkel.thy
+++ 2_Mizar/mizar_fraenkel.thy
@@ -92,67 +92,62 @@ assumes [ex]: "inhabited(L)" "sethood_prop(L)" shows "?X"
 proof
   (induct rule: func_means_property[OF Fraenkel1_def, of L P Q, case_names existence uniqueness])
   case existence
-      obtain X where
-        SetH[ty]:"X be set" and Prop: " for x being object holds (x be L \<longleftrightarrow> x in X)" using sethood assms by auto
-      let ?QQ = "\<lambda>x y. x=y \<and> Q(x)"
-      have A1: "for x,y,z being object holds
-          ?QQ (x,y) \<and> ?QQ (x,z) \<longrightarrow> y = z" by simp
-      obtain XQ where
-         A2[ty]:"XQ be set" and
-         A3:"for x being object holds x in XQ iff
-                 (ex y being object st y in X \<and> ?QQ (y,x))"
-          using tarski_0_sch_1[OF SetH A1] by auto
-      let ?R = "\<lambda>x y. y = P (x)"
-      have A4: "for x,y,z being object holds
-          ?R (x,y) \<and> ?R (x,z) \<longrightarrow> y = z" by simp
-      obtain IT where
-      A5:"IT be set" "for x being object holds (x in IT iff
-         (ex y being object st y in XQ \<and> ?R (y,x)))"
-           using tarski_0_sch_1[OF A2 A4] by auto
-      show "ex IT being set st (for x being object holds (x in IT \<longleftrightarrow> (ex y being L st (x = P (y)  \<and> Q (y) ))))"
-         proof (intro bexI[of _ "IT"] ballI)
-            show "IT be set" using A5 by simp
-            fix x
-            assume "x be object"
-            show "x in IT \<longleftrightarrow> (ex y being L st (x = P (y) \<and> Q (y)))"
-              proof (intro iffI2)
-                 show "x in IT \<longrightarrow> (ex y being L st (x = P (y)  \<and> Q (y) ))"
-                    proof
-                       assume "x in IT"
-                       then obtain y where
-                         "y be object" and A6: "y in XQ \<and> ?R (y,x)" using A5 by auto
-                       show "ex y being L st (x = P (y)  \<and> Q (y) )"
-                         proof (intro bexI[of _ "y"] conjI)
-                           show "x = P (y)" using A6 by simp
-                           obtain z where
-                               "z be object" and A7: "z in X \<and> ?QQ (z,y)" using A3 A6 by auto
-                           show "Q (y)"  "y be L" using A7 Prop by auto
-                         qed simp
-                    qed
-                 show "(ex y being L st (x = P (y)  \<and> Q (y) )) \<longrightarrow> x in IT"
-                    proof
-                      assume "ex y being L st (x = P (y) \<and> Q (y))"
-                      then obtain y where
-                         A8: "y be L" and A9: "x = P (y)" and A10: "Q (y)" by auto
-                      have "y in X" using Prop A8 by auto
-                      hence "y in XQ" using A3 A10 by auto
-                      thus "x in IT" using A5 A9 by auto
-                    qed
-              qed
-         qed simp_all
-    case uniqueness
-    fix IT1 IT2
-    assume B1: "IT1 be set" "IT2 be set" and
-      B2: "for x being object holds (x in IT1 \<longleftrightarrow> (ex y being L st (x = P (y) \<and> Q (y))))" and
-      B3: "for x being object holds (x in IT2 \<longleftrightarrow> (ex y being L st (x = P (y) \<and> Q (y))))"
-    {
-        fix x
-        assume "x be object"
-        have "x in IT1 \<longleftrightarrow> (ex y being L st (x = P (y) \<and> Q (y)))" using B2 by simp
-        hence "x in IT1 \<longleftrightarrow> x in IT2" using B3 by simp
-    }
-    thus "IT1=IT2" using B1 tarski_0_2 by auto
-  qed simp
+    obtain X where SetH: "X be set" and Prop: " for x being object holds (x be L \<longleftrightarrow> x in X)"
+      using sethood assms by auto
+    let ?QQ = "\<lambda>x y. x=y \<and> Q(x)"
+    have A1: "for x, y, z being object holds ?QQ(x, y) \<and> ?QQ(x, z) \<longrightarrow> y = z"
+      by simp
+    obtain XQ where
+      A2: "XQ be set" and
+      A3: "for x being object holds x in XQ iff (ex y being object st y in X \<and> ?QQ(y, x))"
+        using tarski_0_sch_1[OF SetH A1] by auto
+    let ?R = "\<lambda>x y. y = P(x)"
+    have A4: "for x, y, z being object holds ?R(x, y) \<and> ?R(x, z) \<longrightarrow> y = z" by simp
+    obtain IT where A5:
+      "IT be set"
+      "for x being object holds (x in IT iff (ex y being object st y in XQ \<and> ?R(y, x)))"
+      using tarski_0_sch_1[OF A2 A4] by auto
+    show "ex IT being set st (for x being object holds
+      (x in IT \<longleftrightarrow> (ex y being L st (x = P(y) \<and> Q(y)))))"
+    proof (intro bexI[of _ IT] ballI)
+      show "IT be set" using A5 by simp
+      fix x assume "x be object"
+      show "x in IT \<longleftrightarrow> (ex y being L st (x = P(y) \<and> Q(y)))"
+      proof (intro iffI2)
+        show "x in IT \<longrightarrow> (ex y being L st (x = P (y)  \<and> Q (y) ))"
+          proof
+          assume "x in IT"
+          then obtain y where A6: "y in XQ \<and> ?R(y, x)" using A5 by auto
+          show "ex y being L st (x = P(y) \<and> Q(y))"
+          proof (intro bexI[of _ "y"] conjI)
+            show "x = P (y)" using A6 by simp
+            obtain z where "z in X \<and> ?QQ(z, y)" using A3 A6 by auto
+            thus "Q (y)" and "y be L" using Prop by auto
+          qed simp
+        qed
+        show "(ex y being L st (x = P(y) \<and> Q(y))) \<longrightarrow> x in IT"
+        proof
+          assume "ex y being L st (x = P(y) \<and> Q(y))"
+          then obtain y where
+             A8: "y be L" and A9: "x = P (y)" and A10: "Q (y)" by auto
+          have "y in X" using Prop A8 by auto
+          hence "y in XQ" using A3 A10 by auto
+          thus "x in IT" using A5 A9 by auto
+        qed
+      qed
+    qed simp_all
+  case uniqueness
+  fix IT1 IT2 assume
+    B1: "IT1 be set" "IT2 be set" and
+    B2: "for x being object holds (x in IT1 \<longleftrightarrow> (ex y being L st (x = P(y) \<and> Q(y))))" and
+    B3: "for x being object holds (x in IT2 \<longleftrightarrow> (ex y being L st (x = P(y) \<and> Q(y))))"
+  {
+    fix x assume "x be object"
+    have "x in IT1 \<longleftrightarrow> (ex y being L st (x = P(y) \<and> Q(y)))" using B2 by simp
+    hence "x in IT1 \<longleftrightarrow> x in IT2" using B3 by simp
+  }
+  thus "IT1 = IT2" using B1 tarski_0_2 by auto
+qed simp
 
 lemmas Fraenkel_A1_ex = Fraenkel_A1[THEN conjunct2, THEN conjunct2, THEN bspec, simplified, THEN iffD1]
 
@@ -160,35 +155,29 @@ lemma [clus]: "Fraenkel1(P, L, Q) be set"
   using tarski_0_1 by auto
 
 theorem Fraenkel_A1_in:
-   "sethood_prop(M) \<Longrightarrow> x be M \<Longrightarrow> Q(x) \<Longrightarrow> P(x) in {P(x) where x be M : Q(x)}"
-proof-
+  "sethood_prop(M) \<Longrightarrow> x be M \<Longrightarrow> Q(x) \<Longrightarrow> P(x) in {P(x) where x be M : Q(x)}"
+proof -
   assume A:"sethood_prop(M)" "x be M" "Q(x)"
-  hence I:"inhabited(M)" using inhabited_def[of M]  by blast
-    have "ex y be M st P(x) = P(y) \<and> Q(y)" using A I by auto
-  thus "P(x) in {P(x) where x be M : Q(x)}" using Fraenkel_A1 A I
-     by auto
- qed
+  hence I: "inhabited(M)" using inhabited_def[of M] by blast
+  have "ex y be M st P(x) = P(y) \<and> Q(y)" using A I by auto
+  thus "P(x) in {P(x) where x be M : Q(x)}" using Fraenkel_A1 A I by auto
+qed
 
 abbreviation the_set_of_all
-where "the_set_of_all (P, M) \<equiv> Fraenkel1(P, M, (\<lambda>x. True))"
+  where "the_set_of_all(P, M) \<equiv> Fraenkel1(P, M, \<lambda>x. True)"
 
 syntax
   "_SetOfAll" :: "Set \<Rightarrow> vs \<Rightarrow> Ty \<Rightarrow> Set" ("the set-of-all _ where _ be _")
 translations
- "the set-of-all f where x be D" \<rightleftharpoons> "CONST mizar_fraenkel.the_set_of_all((%x. f), D)"
-
-(*
-term "the set-of-all {x} where x be set"
-*)
+ "the set-of-all f where x be D" \<rightleftharpoons> "CONST mizar_fraenkel.the_set_of_all((\<lambda>x. f), D)"
 
 theorem Set_of_All_ex:
- "inhabited(M) \<Longrightarrow> sethood_prop(M) \<Longrightarrow> x in the set-of-all P(x) where x be M \<Longrightarrow> ex y be M st x = P(y)"
-proof-
-  assume A1:"inhabited(M)" "sethood_prop(M)" "x in the set-of-all P(x) where x be M"
-  show "ex y be M st x = P(y)" using Fraenkel_A1_ex[OF A1] by auto
-qed
+  "\<lbrakk>inhabited(M); sethood_prop(M)\<rbrakk> \<Longrightarrow>
+    x in the set-of-all P(x) where x be M \<Longrightarrow> ex y be M st x = P(y)"
+  using Fraenkel_A1_ex by blast
 
 theorem Set_of_All_in:
- "sethood_prop(M) \<Longrightarrow> x be M \<Longrightarrow> P(x) in the set-of-all P(x) where x be M" using Fraenkel_A1_in by auto
+  "sethood_prop(M) \<Longrightarrow> x be M \<Longrightarrow> P(x) in the set-of-all P(x) where x be M"
+  using Fraenkel_A1_in by auto
 
 end
