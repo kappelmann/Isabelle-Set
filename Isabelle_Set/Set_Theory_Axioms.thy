26deeec6eb2a38fd1e6a13cdc4498ae10b3f8381
Josh Chen <jaycech3n@gmail.com>
2019-05-20 12:42:37 +0200

    Slight logical reformulations

--- Isabelle_Set/Set_Theory_Axioms.thy
+++ Isabelle_Set/Set_Theory_Axioms.thy
@@ -1,45 +1,53 @@
 theory Set_Theory_Axioms
-  imports Notation_Cleanup
+imports Notation_Cleanup
+
 begin
 
-subsection \<open> Axiomatic basis of set theory embedded in HOL. \<close>
+section \<open> Axiomatic basis of higher-order Tarski-Grothendieck set theory embedded in HOL. \<close>
 
-
-text \<open> 
-  We axiomatize a type \<open>set\<close>, with the standard set-theoretic operations and axioms.
+text \<open>
+We axiomatize a type \<open>set\<close>, with the standard set-theoretic operations and axioms.
 \<close>
 
-typedecl set 
+typedecl set
 
 axiomatization
-  member :: "set \<Rightarrow> set \<Rightarrow> bool" (infixl "\<in>" 50) and
+  elem :: "set \<Rightarrow> set \<Rightarrow> bool" (infixl "\<in>" 50) and
   empty_set :: set ("{}") and
   Pow :: "set \<Rightarrow> set" and
   Union :: "set \<Rightarrow> set" ("\<Union>_" [90] 90) and
   Repl :: "set \<Rightarrow> (set \<Rightarrow> set) \<Rightarrow> set"
 where
-  elem_induct_axiom: "(\<forall>X. (\<forall>x. x \<in> X \<longrightarrow> P x) \<longrightarrow> P X) \<longrightarrow> P A"
-  and not_mem_empty[simp]: "\<not> x \<in> {}"
-  and Union_axiom[iff]: "x \<in> \<Union>X \<longleftrightarrow> (\<exists>Y. Y \<in> X \<and> x \<in> Y)"
-  and Replacement_axiom[iff]: "y \<in> Repl X F \<longleftrightarrow> (\<exists>x. x \<in> X \<and> y = F x)"
+  elem_induct_axiom: "(\<forall>X. (\<forall>x. x \<in> X \<longrightarrow> P x) \<longrightarrow> P X) \<longrightarrow> (\<forall>X. P X)"
+and
+  empty_axiom [simp]: "\<not>(\<exists>x. x \<in> {})"
+and
+  Union_axiom [iff]: "\<forall>X x. x \<in> \<Union>X \<longleftrightarrow> (\<exists>Y. Y \<in> X \<and> x \<in> Y)"
+and
+  Replacement_axiom [iff]: "\<forall>X y. y \<in> Repl X F \<longleftrightarrow> (\<exists>x. x \<in> X \<and> y = F x)"
 
-text \<open> The following axioms need a definition first: \<close>
+text \<open>Axioms @{thm elem_induct_axiom} and @{thm Replacement_axiom} are axiom schemas in first-order logic.\<close>
 
-definition subset :: "set \<Rightarrow> set \<Rightarrow> bool"  (infixl "\<subseteq>" 50)  \<comment> \<open>subset relation\<close>
+
+text \<open>The following axioms need a definition first:\<close>
+
+definition subset :: "set \<Rightarrow> set \<Rightarrow> bool" (infixl "\<subseteq>" 50) \<comment> \<open>subset relation\<close>
   where subset_def: "A \<subseteq> B \<equiv> (\<forall>x. x \<in> A \<longrightarrow> x \<in> B)"
 
-axiomatization
-  where extensionality: "X \<subseteq> Y \<Longrightarrow> Y \<subseteq> X \<Longrightarrow> X = Y"
-  and Pow_iff: "x \<in> Pow A \<longleftrightarrow> x \<subseteq> A"
+axiomatization where
+  extensionality_axiom: "\<forall>X Y. X \<subseteq> Y \<longrightarrow> Y \<subseteq> X \<longrightarrow> X = Y"
+and
+  Pow_axiom: "\<forall>A x. x \<in> Pow A \<longleftrightarrow> x \<subseteq> A"
 
-text \<open> Finally, we axiomatize the constant \<open>Univ\<close>, which, given a set,
-  returns a Grothendieck universe containing the set.
 
-  This is the Tarski axiom.
+text \<open>
+Finally, we axiomatize the constant \<open>Univ\<close>, which, given a set, returns a Grothendieck universe containing the set.
+
+This is the Tarski axiom.
 \<close>
 
 definition transitive :: "set \<Rightarrow> bool"
-  where "transitive U \<equiv> (\<forall>x. x \<in> U \<longrightarrow> x \<subseteq> U)"
+  where "transitive X \<equiv> (\<forall>x. x \<in> X \<longrightarrow> x \<subseteq> X)"
 
 definition ZF_closed :: "set \<Rightarrow> bool"
   where "ZF_closed U \<equiv> (
@@ -48,11 +56,17 @@ definition ZF_closed :: "set \<Rightarrow> bool"
       (\<forall>X F. X \<in> U \<longrightarrow> (\<forall>x. x \<in> X \<longrightarrow> F x \<in> U) \<longrightarrow> Repl X F \<in> U)
     )"
 
+text \<open>Remark: @{const ZF_closed} is a second-order statement.\<close>
+
+
 axiomatization
   Univ :: "set \<Rightarrow> set"
-  where Univ_member: "N \<in> Univ N"
-  and Univ_transtive: "transitive (Univ N)"
-  and Univ_ZF_closed: "ZF_closed (Univ N)"
+where
+  Univ_member: "X \<in> Univ X"
+and
+  Univ_transitive: "transitive (Univ X)"
+and
+  Univ_ZF_closed: "ZF_closed (Univ X)"
 
 
 end
