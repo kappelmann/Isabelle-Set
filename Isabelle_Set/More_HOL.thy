16a2c85664b7dad3aa304a79dbbfead34dc3b095
Josh Chen <jaycech3n@gmail.com>
2019-05-22 00:30:48 +0200

    Move setup to More_HOL.thy

---
+++ Isabelle_Set/More_HOL.thy
@@ -0,0 +1,44 @@
+theory More_HOL
+imports
+  HOL.HOL
+  "../Soft_Types/Soft_Types_HOL" (* <-- Needs to go before Eisbach, for some reason *)
+  "HOL-Eisbach.Eisbach"
+  "HOL-Eisbach.Eisbach_Tools"
+
+begin
+
+
+subsection \<open>Notational setup\<close>
+
+text \<open>Get rid of HOL-specific syntax which would conflict with set-theoretic syntax.\<close>
+
+no_notation (ASCII)
+  Not ("~ _" [40] 40) and
+  conj (infixr "&" 35) and
+  disj (infixr "|" 30) and
+  implies (infixr "-->" 25) and
+  not_equal (infixl "~=" 50)
+
+no_syntax "_Let" :: "[letbinds, 'a] \<Rightarrow> 'a" ("(let (_)/ in (_))" 10)
+
+
+subsection \<open>Additional logical rules\<close>
+
+lemma disjCI2: "(\<not>A \<Longrightarrow> B) \<Longrightarrow> A \<or> B"
+  by blast
+
+text \<open>Provide more support for @{term undefined}.\<close>
+
+(* What we'd really like is to have a rule/schema saying that whenever "undefined" is
+an argument to any function, then the result of the evaluation is also undefined.
+For the moment I don't know how to do this generally, hence the following.
+
+Observe that the result of comparing an undefined with any other value is itself undefined.
+*)
+axiomatization where
+  undefined1: "\<And>f. f undefined = undefined" and
+  undefined2: "\<And>f x. f undefined x = undefined" and
+  undefined2': "\<And>f x. f x undefined = undefined"
+
+
+end
