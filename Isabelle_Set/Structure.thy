e9dcc85942a70ecc1dec85f161466ea9b0f5d453
Alexander Krauss <alexander.krauss@qaware.de>
2019-06-27 12:59:31 +0100

    Idiomatic use of tables.

--- Isabelle_Set/Structure.thy
+++ Isabelle_Set/Structure.thy
@@ -22,60 +22,47 @@ structure Labels: sig
   val string_to_hash : string -> term
 end = struct
 
-val char_hash : term HashArray.hash = HashArray.hash 36
+  (* TODO: binary encoding; proper symbol coverage *)
+  fun hash i = funpow i (fn t => @{const "Succ"} $ t) @{term "{}"}
+  val get_hash = Symtab.make [
+    ("0", hash 0),
+    ("1", hash 1),
+    ("2", hash 2),
+    ("3", hash 3),
+    ("4", hash 4),
+    ("5", hash 5),
+    ("6", hash 6),
+    ("7", hash 7),
+    ("8", hash 8),
+    ("9", hash 9),
+    ("a", hash 10),
+    ("b", hash 11),
+    ("c", hash 12),
+    ("d", hash 13),
+    ("e", hash 14),
+    ("f", hash 15),
+    ("g", hash 16),
+    ("h", hash 17),
+    ("i", hash 18),
+    ("j", hash 19),
+    ("k", hash 20),
+    ("l", hash 21),
+    ("m", hash 22),
+    ("n", hash 23),
+    ("o", hash 24),
+    ("p", hash 25),
+    ("q", hash 26),
+    ("r", hash 27),
+    ("s", hash 28),
+    ("t", hash 29),
+    ("u", hash 30),
+    ("v", hash 31),
+    ("w", hash 32),
+    ("x", hash 33),
+    ("y", hash 34),
+    ("z", hash 35)]
+  |> Symtab.lookup
 
-(* Populate char_hash *)
-val _ =
-let
-  (* We hash characters as set-theoretic naturals *)
-  fun hash i =
-    let val O = @{term "{}"}
-    in
-      if i = 0 then O
-      else @{const "Succ"} $ (hash (i-1))
-    end
-
-  fun upd (s, v) = HashArray.update (char_hash, s, v)  (* cf. Pure/General/table.ML *)
-in
-  upd ("0", hash 0);
-  upd ("1", hash 1);
-  upd ("2", hash 2);
-  upd ("3", hash 3);
-  upd ("4", hash 4);
-  upd ("5", hash 5);
-  upd ("6", hash 6);
-  upd ("7", hash 7);
-  upd ("8", hash 8);
-  upd ("9", hash 9);
-  upd ("a", hash 10);
-  upd ("b", hash 11);
-  upd ("c", hash 12);
-  upd ("d", hash 13);
-  upd ("e", hash 14);
-  upd ("f", hash 15);
-  upd ("g", hash 16);
-  upd ("h", hash 17);
-  upd ("i", hash 18);
-  upd ("j", hash 19);
-  upd ("k", hash 20);
-  upd ("l", hash 21);
-  upd ("m", hash 22);
-  upd ("n", hash 23);
-  upd ("o", hash 24);
-  upd ("p", hash 25);
-  upd ("q", hash 26);
-  upd ("r", hash 27);
-  upd ("s", hash 28);
-  upd ("t", hash 29);
-  upd ("u", hash 30);
-  upd ("v", hash 31);
-  upd ("w", hash 32);
-  upd ("x", hash 33);
-  upd ("y", hash 34);
-  upd ("z", hash 35)
-end
-
-fun get_hash str = HashArray.sub (char_hash, str)
 
 (* Convert strings to ordered tuples of the hashes of each letter. *)
 fun string_to_hash str =
