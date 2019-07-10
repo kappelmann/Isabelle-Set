e33d377cbffa611281448d3d82151338a5a946f2
Alexander Krauss <alexander.krauss@qaware.de>
2019-07-10 09:10:57 +0100

    Change Isabelle download source to patches repository on Bitbucket.

--- .circleci/prepare_isabelle.sh
+++ .circleci/prepare_isabelle.sh
@@ -5,11 +5,11 @@
 # To be used in the build environment only.
 
 ISABELLE_VERSION=$(cat ~/project/ISABELLE_VERSION)
-
 if [ ! -d "$HOME/Isabelle" ]; then
+
   mkdir ~/Isabelle;
   cd ~/Isabelle;
-  curl -sS https://isabelle.in.tum.de/repos/isabelle/archive/$ISABELLE_VERSION.tar.gz | tar -xz --strip-components=1;
+  curl -sS https://bitbucket.org/akrauss/isabelle-soft-types/get/$ISABELLE_VERSION.tar.gz | tar -xz --strip-components=1;
   ~/Isabelle/bin/isabelle components -I;
   ~/Isabelle/bin/isabelle components -a;
   # Do not eat too much memory, to avoid being killed.
