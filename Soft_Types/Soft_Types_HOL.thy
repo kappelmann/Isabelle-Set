dca5964f20fd340b15c7190eb12d8d51c2699505
Alexander Krauss <alexander.krauss@qaware.de>
2019-07-30 13:14:14 +0100

    Allow further facts to be passed to derivation, and pipe in context from the simplifier.

--- Soft_Types/Soft_Types_HOL.thy
+++ Soft_Types/Soft_Types_HOL.thy
@@ -220,12 +220,12 @@ declare Pi_typeI [bderive]
 subsection \<open>Methods\<close>
 
 ML \<open>
-fun full_discharge_types_tac add_tms ctxt =
+fun full_discharge_types_tac more_facts add_tms ctxt =
   let
     val type_tac =
       let val refine_tac = resolve_tac ctxt [@{thm Int_typeI}, @{thm adjI}]
       in
-        (TRY o REPEAT o refine_tac) THEN' Derivation.discharge_type_tac ctxt add_tms
+        (TRY o REPEAT o refine_tac) THEN' Derivation.discharge_type_tac ctxt more_facts add_tms
       end
     val backward_tac = resolve_tac ctxt
       (Named_Theorems.get ctxt \<^named_theorems>\<open>backderivation_rules\<close>)
@@ -239,7 +239,7 @@ fun full_discharge_types_tac add_tms ctxt =
 method_setup discharge_types =
   \<open>Scan.optional (Scan.lift (Args.add -- Args.colon) |-- Scan.repeat Args.term) [] >>
     (fn add_tms => fn ctxt => SIMPLE_METHOD (
-      (REPEAT1 (CHANGED (ALLGOALS (full_discharge_types_tac add_tms ctxt))))))\<close>
+      (REPEAT1 (CHANGED (ALLGOALS (full_discharge_types_tac [] add_tms ctxt))))))\<close>
 
 
 named_theorems squash
@@ -268,7 +268,7 @@ let
       THEN SOLVED' (SUBGOAL (fn (t, i) =>
         (Output.tracing (Syntax.string_of_term ctxt t);
         if Soft_Type.is_typing t
-        then full_discharge_types_tac [] ctxt i
+        then full_discharge_types_tac (Simplifier.prems_of ctxt) [] ctxt i
         else no_tac)
       )) i
     end
