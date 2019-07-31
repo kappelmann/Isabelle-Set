a5ad28f30f0ea1541f611d2cfa69a19673c05114
Josh Chen <josh@joshchen.io>
2019-07-31 16:03:35 +0200

    rename Sum_Type to avoid clashes with Sum soft type later

--- Isabelle_Set/Sum.thy
+++ Isabelle_Set/Sum.thy
@@ -1,13 +1,13 @@
-theory Sum_Type
+theory Sum
   imports Pair Universe
 begin
 
 definition "Inl a = \<langle>{}, a\<rangle>"
 definition "Inr b = \<langle>{{}}, b\<rangle>"
-definition "Sum_Type A B = Repl A Inl \<union> Repl B Inr"
+definition "Sum A B = Repl A Inl \<union> Repl B Inr"
 
-lemma sum_type_iff: "x \<in> Sum_Type A B \<longleftrightarrow> (\<exists>a \<in> A. x = Inl a) \<or> (\<exists>b \<in> B. x = Inr b)"
-  unfolding Sum_Type_def Inl_def Inr_def by blast
+lemma sum_type_iff: "x \<in> Sum A B \<longleftrightarrow> (\<exists>a \<in> A. x = Inl a) \<or> (\<exists>b \<in> B. x = Inr b)"
+  unfolding Sum_def Inl_def Inr_def by blast
 
 lemma
   Inl_inject[iff]: "Inl x = Inl y \<longleftrightarrow> x = y" and
@@ -26,16 +26,16 @@ lemma
 
 
 lemma sum_elim[case_names Inl Inr]:
-  assumes "x \<in> Sum_Type A B"
+  assumes "x \<in> Sum A B"
   obtains a where "a \<in> A" "x = Inl a" | b where "b \<in> B" "x = Inr b"
-  using assms unfolding Sum_Type_def by blast
+  using assms unfolding Sum_def by blast
 
 
-lemma Inl_type [type]: "Inl : element A \<Rightarrow> element (Sum_Type A B)"
-  unfolding Inl_def Sum_Type_def by squash_types blast
+lemma Inl_type [type]: "Inl : element A \<Rightarrow> element (Sum A B)"
+  unfolding Inl_def Sum_def by squash_types blast
 
-lemma Inr_type [type]: "Inr : element B \<Rightarrow> element (Sum_Type A B)"
-  unfolding Inr_def Sum_Type_def by squash_types blast
+lemma Inr_type [type]: "Inr : element B \<Rightarrow> element (Sum A B)"
+  unfolding Inr_def Sum_def by squash_types blast
 
 lemma Inl_Univ [derive]: "x : element (Univ A) \<Longrightarrow> Inl x : element (Univ A)"
   unfolding Inl_def 
