4b8d1f26d61f8f446ff7d8575c484290a6fa5d2a
Josh Chen <josh@joshchen.io>
2019-08-26 14:05:49 +0200

    category theory experiments

---
+++ Isabelle_Set/Category.thy
@@ -0,0 +1,74 @@
+section \<open>Basic category theory\<close>
+
+theory Category
+imports Object
+
+begin
+
+object QuasiCategory "X::set" is "\<lparr> (@obj obj) (@hom hom) (@comp comp) (@id id).
+  obj  : non-empty \<sqdot> element X \<and>
+  hom  : element (obj \<rightarrow> obj \<rightarrow> X) \<and>
+  comp : element (\<Prod>A \<in> obj. \<Prod>B \<in> obj. \<Prod>C \<in> obj. (hom`A`B \<rightarrow> hom`B`C \<rightarrow> hom`A`C)) \<and>
+  id   : element (\<Prod>A \<in> obj. (hom`A`A)) \<and>
+
+  \<comment>\<open>identity morphisms\<close>
+  (\<forall>A \<in> obj. id`A \<in> hom`A) \<and>
+  (\<forall>A \<in> obj. \<forall>B \<in> obj.
+    (\<forall>f \<in> hom`A`B. comp`f`(id`A) = f) \<and> (\<forall>g \<in> hom`B`A. comp`(id`B)`g = g)) \<and>
+
+  \<comment>\<open>associativity\<close>
+  (\<forall>A \<in> obj. \<forall>B \<in> obj. \<forall>C \<in> obj. \<forall>D \<in> obj.
+    \<forall>f \<in> hom`A`B. \<forall>g \<in> hom`B`C. \<forall>h \<in> hom`C`D.
+      comp`(comp`h`g)`f = comp`h`(comp`g`f))
+\<rparr>"
+
+definition obj :: \<open>set \<Rightarrow> set\<close> ("obj\<^bsub>_\<^esub>")
+  where "obj\<^bsub>C\<^esub> = C[@obj]"
+
+definition hom :: \<open>set \<Rightarrow> set \<Rightarrow> set \<Rightarrow> set\<close> ("hom\<^bsub>_\<^esub>")
+  where "hom\<^bsub>C\<^esub> A B = C[@hom]`A`B"
+
+definition SmallCategory
+  where SmallCategory_typedef: "SmallCategory = QuasiCategory (Univ {})"
+
+definition Category
+  where Category_typedef: "Category = QuasiCategory (Univ (Univ {}))"
+
+
+subsection \<open>Examples of categories\<close>
+
+text \<open>Sets in the lowest universe.\<close>
+
+definition Set_cat ("\<S>et")
+  where "\<S>et = \<lparr>
+    @obj = Univ {},
+    @hom = \<lambda>A \<in> @obj. \<lambda>B \<in> @obj. (A \<rightarrow> B),
+    @comp = \<lambda>A \<in> @obj. \<lambda>B \<in> @obj. \<lambda>C \<in> @obj. \<lambda>f \<in> @hom`A`B. \<lambda>g \<in> @hom`B`C. (g \<circ> f),
+    @id = \<lambda>A \<in> @obj. \<lambda>x \<in> @hom`A`A. x
+  \<rparr>"
+
+text \<open>Too much low-level manipulation in the following proof...\<close>
+
+declare [[trace_soft_types]]
+
+lemma Set_cat_type: "\<S>et : Category"
+unfolding Category_typedef QuasiCategory_typedef
+proof auto
+  show "\<S>et[@obj] : non-empty \<sqdot> element (Univ (Univ {}))"
+    apply (simp add: Set_cat_def selector_def; subst apply_cons1)
+    apply (auto; strings)
+    apply discharge_types
+    apply (rule Univ_nonempty)
+    done
+
+  show "\<S>et[@hom] : element (\<S>et[@obj] \<rightarrow> \<S>et[@obj] \<rightarrow> Univ (Univ {}))"
+    apply (simp add: Set_cat_def selector_def; (subst (0 2) apply_cons1))
+    apply (auto; strings)+
+    apply (subst cons_commute, subst apply_cons1)
+    apply (auto; strings)+
+    apply squash_types
+    (* etc. etc. *)
+oops
+
+
+end
