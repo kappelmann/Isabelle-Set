d78790219ce8160d003172c4ed67de5fc73dc7a6
Alexander Krauss <alexander.krauss@qaware.de>
2019-09-12 11:44:47 +0100

    Properly deal with side conditions in type class refinement.

--- Isabelle_Set/Test_Examples/Matrix.thy
+++ Isabelle_Set/Test_Examples/Matrix.thy
@@ -12,7 +12,7 @@ definition
   "Matrix A m n = {0..< m} \<rightarrow> {0..< n} \<rightarrow> A"
 
 definition
-  "matrix_idx M i j = M`i`j"
+  "matrix_idx (A::set) (m::set) (n::set) M i j = M`i`j"
 
 lemma upto_type [type]: "upto : nat \<Rightarrow> subset \<nat>"
   unfolding upto_def by discharge_types
@@ -21,7 +21,7 @@ lemma Matrix_type [type]: "Matrix : set \<Rightarrow> nat \<Rightarrow> nat \<Ri
   by discharge_types
 
 lemma matrix_idx_type [type]:
-  "matrix_idx : element (Matrix A m n) \<Rightarrow> element {0..< m} \<Rightarrow> element {0..< n} \<Rightarrow> element A"
+  "matrix_idx : (A : set) \<Rightarrow> (m : nat) \<Rightarrow> (n : nat) \<Rightarrow> element (Matrix A m n) \<Rightarrow> element {0..< m} \<Rightarrow> element {0..< n} \<Rightarrow> element A"
   unfolding Matrix_def matrix_idx_def
   by discharge_types
 
@@ -29,10 +29,10 @@ lemma matrix_idx_type [type]:
 text \<open>Given the dimensions and a function mapping indices to values, construct a matrix:\<close>
 
 definition  
-  "matrix_comp n m f = \<lambda>i \<in> {0..< n}. \<lambda>j \<in> {0..< m}. f i j"
+  "matrix_comp (A::set) n m f = \<lambda>i \<in> {0..< n}. \<lambda>j \<in> {0..< m}. f i j"
 
 lemma matrix_comp_type [type]:
-  "matrix_comp : (m : nat) \<Rightarrow> (n : nat) \<Rightarrow> (element {0..< m} \<Rightarrow> element {0..< n} \<Rightarrow> element A) 
+  "matrix_comp : (A : set) \<Rightarrow> (m : nat) \<Rightarrow> (n : nat) \<Rightarrow> (element {0..< m} \<Rightarrow> element {0..< n} \<Rightarrow> element A) 
     \<Rightarrow> element (Matrix A m n)"
   unfolding matrix_comp_def Matrix_def
   by discharge_types
@@ -44,7 +44,7 @@ text \<open>Class instance:\<close>
 
 definition
   "Plus_Matrix A n m p = \<lparr> @plus = (\<lambda>M\<in>Matrix A n m. \<lambda>N\<in>Matrix A n m.
-     matrix_comp n m (\<lambda> i j. plus p (matrix_idx M i j) (matrix_idx N i j))) \<rparr>"
+     matrix_comp A n m (\<lambda> i j. plus p (matrix_idx A n m M i j) (matrix_idx A n m N i j))) \<rparr>"
 
 lemma Plus_Matrix_type [type]:
   "Plus_Matrix : (A : set) \<Rightarrow> (n : nat) \<Rightarrow> (m : nat) \<Rightarrow> Plus A \<Rightarrow> Plus (Matrix A n m)"
@@ -56,13 +56,13 @@ lemma Plus_Matrix_instance [type_instance]:
   "n : nat \<Longrightarrow> m : nat \<Longrightarrow> p : Plus A \<Longrightarrow> Plus_Matrix A n m p : Plus (Matrix A n m)"
   by discharge_types
 
-
-(*
-
 declare [[auto_elaborate]]
+thm matrix_idx_type
 
-lemma "matrix_idx (M + N) i j = matrix_idx M i j + matrix_idx N i j"
 
-*)
+lemma "matrix_idx A n m (M + N) i j = matrix_idx A n m M i j + matrix_idx A n m N i j"
+  print_types
+  oops
+
 
 end
