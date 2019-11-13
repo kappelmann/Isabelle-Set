b4ff06068b9fb15829c3dedeb3ac7b5bc236bcee
Josh Chen <josh@joshchen.io>
2019-11-13 16:05:11 +0100

    1. Remove subtype constant, can probably simply use a named_theorem. 2. Some refactoring. NOTE this build fails.

--- Soft_Types/Soft_Types_HOL.thy
+++ Soft_Types/Soft_Types_HOL.thy
@@ -2,7 +2,6 @@ section \<open>Soft types for HOL\<close>
 
 text \<open>
   This theory introduces a generic notion of soft types based on HOL predicates.
-
   It contains the basic definitions and technical tool setup.
 \<close>
 
@@ -23,117 +22,88 @@ declare [[eta_contract=false]]
 
 subsection \<open>Basic definitions\<close>
 
-text \<open>Soft types are just predicates, but expressed as a different type:\<close>
+text \<open>Soft types are "just" predicates wrapped up in a constructor:\<close>
 
 typedecl 'a type
 
 axiomatization
-  Type :: "('a \<Rightarrow> bool) \<Rightarrow> 'a type" and
-  pred_of :: "'a type \<Rightarrow> 'a \<Rightarrow> bool"
+  type :: "('a \<Rightarrow> bool) \<Rightarrow> 'a type" and
+  pred :: "'a type \<Rightarrow> 'a \<Rightarrow> bool"
 where
-  pred_of_type: "pred_of (Type t) \<equiv> t"
+  pred_of_type [simp]: "pred (type P) \<equiv> P"
 
-definition has_type :: "'a \<Rightarrow> 'a type \<Rightarrow> bool" ("(1_ :/ _)" [46, 46] 45)
-  where "x : T \<equiv> pred_of T x"
+definition has_type :: "'a \<Rightarrow> 'a type \<Rightarrow> bool" (infix ":" 45)
+  where "x : T \<equiv> pred T x"
 
-lemma has_type_iff: "x : Type P \<longleftrightarrow> P x"
-  unfolding has_type_def by (simp only: pred_of_type)
+lemma
+  has_typeI: "P x \<Longrightarrow> x : type P" and
+  has_typeE: "x : type P \<Longrightarrow> P x"
+  unfolding has_type_def by auto
 
 
 subsection \<open>Bounded quantifiers\<close>
 
-definition Soft_Ball :: "'a type \<Rightarrow> ('a \<Rightarrow> bool) \<Rightarrow> bool"
-  where "Soft_Ball A P \<equiv> (\<forall>x. x : A \<longrightarrow> P x)"
+definition SBall :: "'a type \<Rightarrow> ('a \<Rightarrow> bool) \<Rightarrow> bool"
+  where "SBall A P \<equiv> (\<forall>x. x : A \<longrightarrow> P x)"
 
-definition Soft_Bex :: "'a type \<Rightarrow> ('a \<Rightarrow> bool) \<Rightarrow> bool"
-  where "Soft_Bex A P \<equiv> (\<exists>x. x : A \<and> P x)"
+definition SBex :: "'a type \<Rightarrow> ('a \<Rightarrow> bool) \<Rightarrow> bool"
+  where "SBex A P \<equiv> (\<exists>x. x : A \<and> P x)"
 
 syntax
-  "_Soft_Ball" :: "[pttrn, 'a type, bool] \<Rightarrow> bool"  ("(3\<forall>_ : _./ _)" 10)
-  "_Soft_Bex"  :: "[pttrn, 'a type, bool] \<Rightarrow> bool"  ("(3\<exists>_ : _./ _)" 10)
+  "_SBall" :: "[pttrn, 'a type, bool] \<Rightarrow> bool"  ("(3\<forall>_ : _./ _)" 10)
+  "_SBex"  :: "[pttrn, 'a type, bool] \<Rightarrow> bool"  ("(3\<exists>_ : _./ _)" 10)
 translations
-  "\<forall>x : A. P" \<rightleftharpoons> "CONST Soft_Ball A (\<lambda>x. P)"
-  "\<exists>x : A. P" \<rightleftharpoons> "CONST Soft_Bex A (\<lambda>x. P)"
+  "\<forall>x : A. P" \<rightleftharpoons> "CONST SBall A (\<lambda>x. P)"
+  "\<exists>x : A. P" \<rightleftharpoons> "CONST SBex A (\<lambda>x. P)"
+
+lemma SBallI [intro!]: "(\<And>x. x : A \<Longrightarrow> P x) \<Longrightarrow> \<forall>x : A. P x"
+  unfolding SBall_def by auto
+
+lemma SBallE [elim]: "\<lbrakk>\<forall>x : A. P x; \<And>x. (x : A \<Longrightarrow> P x) \<Longrightarrow> R\<rbrakk> \<Longrightarrow> R"
+  unfolding SBall_def by auto
+
+lemma SBallE' [elim]: "\<lbrakk>\<forall>x : A. P x; x : A\<rbrakk> \<Longrightarrow> P x"
+  unfolding SBall_def by auto
+
+lemma SBexI [intro]: "\<lbrakk>x : A; P x\<rbrakk> \<Longrightarrow> \<exists>x : A. P x"
+  unfolding SBex_def by auto
+
+lemma SBexE [elim]: "\<lbrakk>\<exists>x : A. P x; \<And>x. \<lbrakk>x : A; P x\<rbrakk> \<Longrightarrow> R\<rbrakk> \<Longrightarrow> R"
+  unfolding SBex_def by auto
 
 
-lemma Soft_BallI [intro!]: "(\<And>x. x : A \<Longrightarrow> P x) \<Longrightarrow> \<forall>x : A. P x"
-  unfolding Soft_Ball_def by auto
+subsection \<open>\<Pi> types\<close>
 
-lemma Soft_BallE [elim]: "\<lbrakk>\<forall>x : A. P x; \<And>x. (x : A \<Longrightarrow> P x) \<Longrightarrow> R\<rbrakk> \<Longrightarrow> R"
-  unfolding Soft_Ball_def by auto
-
-lemma Soft_BallE' [elim]: "\<lbrakk>\<forall>x : A. P x; x : A\<rbrakk> \<Longrightarrow> P x"
-  unfolding Soft_Ball_def by auto
-
-lemma Soft_BexI [intro]: "\<lbrakk>x : A; P x\<rbrakk> \<Longrightarrow> \<exists>x : A. P x"
-  unfolding Soft_Bex_def by auto
-
-lemma Soft_BexE [elim]: "\<lbrakk>\<exists>x : A. P x; \<And>x. \<lbrakk>x : A; P x\<rbrakk> \<Longrightarrow> R\<rbrakk> \<Longrightarrow> R"
-  unfolding Soft_Bex_def by auto
-
-subsection \<open>Pi types\<close>
-
-text \<open>Soft-types for meta-level functions.\<close>
+text \<open>Dependent function soft type for HOL lambda terms.\<close>
 
 definition Pi_type :: "'a type \<Rightarrow> ('a \<Rightarrow> 'b type) \<Rightarrow> ('a \<Rightarrow> 'b) type"
-  where Pi_typedef: "Pi_type A B \<equiv> Type (\<lambda>f. \<forall>x : A. f x : B x)"
-
-abbreviation function_type :: "'a type \<Rightarrow> 'b type \<Rightarrow> ('a \<Rightarrow> 'b) type"
-  where "function_type A B \<equiv> Pi_type A (\<lambda>_. B)"
+  where "Pi_type A B \<equiv> type (\<lambda>f. \<forall>x : A. f x : B x)"
 
 syntax
   "_telescope" :: "logic \<Rightarrow> logic \<Rightarrow> logic"  (infixr "\<Rightarrow>" 50)
 translations
   "(x : A) \<Rightarrow> B" \<rightleftharpoons> "CONST Pi_type A (\<lambda>x. B)"
-  "A \<Rightarrow> B" \<rightleftharpoons> "CONST function_type A B"
+  "A \<Rightarrow> B" \<rightleftharpoons> "CONST Pi_type A (\<lambda>_. B)"
 
-text \<open>
-We write \<^term>\<open>(x : A) \<Rightarrow> B x\<close> for the dependent function type and simply \<^term>\<open>A \<Rightarrow> B\<close> for
-the non-dependent version.
-\<close>
+lemma Pi_typeI:
+  "(\<forall>x. x : A \<longrightarrow> f x : B x) \<Longrightarrow> f : (x : A) \<Rightarrow> B x"
+  unfolding Pi_type_def by (auto intro: has_typeI)
 
-lemma Pi_type_iff: "f : (x : A) \<Rightarrow> B x \<longleftrightarrow> (\<forall>x. x : A \<longrightarrow> f x : B x)"
-  unfolding Pi_typedef by (auto iff: has_type_iff)
-
-lemma
-  Pi_typeI [intro!]: "(\<And>x. x : A \<Longrightarrow> f x : B x) \<Longrightarrow> f : (x : A) \<Rightarrow> B x" and
-
-  Pi_typeE [elim]: "\<lbrakk>f : (x : A) \<Rightarrow> B x; x : A\<rbrakk> \<Longrightarrow> f x : B x"
-
-  by (auto iff: Pi_type_iff)
+lemma Pi_typeE [elim]:
+  "\<lbrakk>f : (x : A) \<Rightarrow> B x; x : A\<rbrakk> \<Longrightarrow> f x : B x"
+  unfolding Pi_type_def by (auto dest: has_typeE)
 
 
-subsection \<open>Intersections\<close>
+subsection \<open>Intersection types\<close>
 
 definition Int_type :: "'a type \<Rightarrow> 'a type \<Rightarrow> 'a type" (infixl "\<bar>" 55)
-  where Int_typedef: "A \<bar> B \<equiv> Type (\<lambda>x. x : A \<and> x : B)"
-
-lemma Int_type_iff: "x : A \<bar> B \<longleftrightarrow> x : A \<and> x : B"
-  unfolding Int_typedef by (simp only: has_type_iff)
+  where "A \<bar> B \<equiv> type (\<lambda>x. x : A \<and> x : B)"
 
 lemma
-  Int_typeI [intro]: "x : A \<Longrightarrow> x : B \<Longrightarrow> x : A \<bar> B" and
-
-  Int_typeD1: "x : A \<bar> B \<Longrightarrow> x : A" and
-
-  Int_typeD2: "x : A \<bar> B \<Longrightarrow> x : B"
-
-  by (auto iff: Int_type_iff)
-
-
-subsection \<open>Subtypes\<close>
-
-definition subtype :: "'a type \<Rightarrow> 'a type \<Rightarrow> bool" (infix "\<prec>" 50)
-  where subtype_iff:  "A \<prec> B \<longleftrightarrow> (\<forall>x : A. x : B)"
-
-lemma subtypeI [intro]: "(\<And>x. x : A \<Longrightarrow> x : B) \<Longrightarrow> A \<prec> B"
-  by (auto iff: subtype_iff)
-
-lemma subtypeE [elim]: "\<lbrakk>A \<prec> B; \<And>x. (x : B \<Longrightarrow> P); x : A\<rbrakk> \<Longrightarrow> P"
-  by (auto dest!: Soft_BallI iff: subtype_iff)
-
-lemma subtypeE' [elim]: "\<lbrakk>A \<prec> B; x : A\<rbrakk> \<Longrightarrow> x : B"
-  by (auto iff: subtype_iff)
+  Int_typeI: "x : A \<Longrightarrow> x : B \<Longrightarrow> x : A \<bar> B" and
+  Int_typeE1: "x : A \<bar> B \<Longrightarrow> x : A" and
+  Int_typeE2: "x : A \<bar> B \<Longrightarrow> x : B"
+  unfolding Int_type_def by (auto intro: has_typeI dest: has_typeE)
 
 
 subsection \<open>The ``any'' type\<close>
