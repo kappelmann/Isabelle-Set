c57efdda3b8854c5c1087f115acb4ebcbba548d1
Josh Chen <josh@joshchen.io>
2020-03-02 17:01:52 +0100

    1. Axiomatized natrec. 2. Monoid structure of (nat, +) done

--- Isabelle_Set/Nat.thy
+++ Isabelle_Set/Nat.thy
@@ -1,7 +1,7 @@
 chapter \<open>Natural numbers\<close>
 
 theory Nat
-imports Ordinal Function
+imports Ordinal Function Monoid
 
 begin
 
@@ -9,19 +9,23 @@ text \<open>The basic results have already been shown for \<nat> = \<omega>.\<cl
 
 definition nat ("\<nat>") where "\<nat> = \<omega>"
 
-notation emptyset ("0")
+definition "nat_zero \<equiv> {}"
+
+no_notation zero_implicit ("0")
+notation nat_zero ("0")
 
 lemmas
-  nat_unfold = omega_unfold[folded nat_def] and
-  zero_nat [simp] = empty_in_omega[folded nat_def] and
+  nat_unfold = omega_unfold[folded nat_def nat_zero_def] and
+  zero_nat [simp] = empty_in_omega[folded nat_def nat_zero_def] and
   succ_nat [simp] = succ_omega[folded nat_def] and
-  nat_cases = omega_cases[folded nat_def] and
-  nat_induct [case_names 0 induct, induct set: nat] = omega_induct[folded nat_def] and
-  nat_elems = omega_elems[folded nat_def] and
+  nat_cases = omega_cases[folded nat_def nat_zero_def] and
+  nat_induct [case_names 0 induct, induct set: nat] =
+    omega_induct[folded nat_def nat_zero_def] and
+  nat_elems = omega_elems[folded nat_def nat_zero_def] and
   pred_nat [simp] = pred_omega[folded nat_def] and
-  pred_0 = pred_empty and
+  pred_0 = pred_empty[folded nat_zero_def] and
   pred_succ [simp] = Ordinal.pred_succ[folded nat_def] and
-  succ_pred [simp] = Ordinal.succ_pred[folded nat_def]
+  succ_pred [simp] = Ordinal.succ_pred[folded nat_def nat_zero_def]
 
 
 section \<open>\<nat> as a type\<close>
@@ -36,7 +40,7 @@ lemmas Nat_induct = nat_induct
 lemmas Nat_cases = nat_cases[simplified element_type_iff]
 
 lemma Nat_Ord [derive]: "x : Nat \<Longrightarrow> x : Ord"
-  by (induct x rule: Nat_induct) (auto intro: succ_Ord)
+  by (induct x rule: Nat_induct) (auto intro: succ_Ord simp: nat_zero_def)
 
 lemma
   zero_type [type]: "0 : Nat" and
@@ -55,7 +59,8 @@ lemmas
 
 lemma succ_lt_monotone [intro]:
   "\<lbrakk>n: Nat; m < n\<rbrakk> \<Longrightarrow> succ m < succ n"
-  unfolding lt_def by (induction n rule: Nat_induct) (auto simp: succ_def)
+  unfolding lt_def
+  by (induction n rule: Nat_induct) (auto simp: succ_def nat_zero_def)
 
 lemma lt_trichotomy:
   "\<lbrakk>m: Nat; n: Nat\<rbrakk> \<Longrightarrow> m < n \<or> m = n \<or> n < m"
@@ -84,10 +89,11 @@ lemma succ_lt_le_monotone:
   unfolding le_def using succ_lt_monotone by auto
 
 lemma lt_0 [simp]: "n : Nat \<Longrightarrow> 0 < succ n"
-  unfolding lt_def nat_def by unfold_types (fact omega_empty_in_succ)
+  unfolding lt_def nat_def nat_zero_def
+  by unfold_types (fact omega_empty_in_succ)
 
 lemma zero_ltE [elim]: "n < 0 \<Longrightarrow> P"
-  unfolding lt_def by auto
+  unfolding lt_def nat_zero_def by auto
 
 lemma le_0 [simp]: "n \<le> 0 \<Longrightarrow> n = 0"
   unfolding le_def by auto
@@ -153,7 +159,7 @@ lemmas upto_iff_le = le_iff_upto[symmetric]
 
 lemma [derive]: "n: Nat \<Longrightarrow> 0: element {0, ..., n}"
   by
-    (simp add: upto_eq_succ nat_def, unfold_types)
+    (simp add: upto_eq_succ nat_def nat_zero_def, unfold_types)
     (auto intro: omega_empty_in_succ)
 
 lemma
@@ -217,286 +223,75 @@ lemma cons_upto_FunctionI [intro]:
 
 section \<open>Recursion\<close>
 
-text \<open>Well-founded recursion on Nat\<close>
+text \<open>Recursion on Nat. Axiomatized, for now.\<close>
 
-theorem Nat_recursion:
-  assumes "f : element X \<Rightarrow> element X"
-      and "x\<^sub>0 : element X"
-  obtains F where
-    "F : Nat \<Rightarrow> element X"
-    "F 0 = x\<^sub>0"
-    "\<And>n. n: Nat \<Longrightarrow> F (succ n) = f (F n)"
+axiomatization natrec :: \<open>set \<Rightarrow> (set \<Rightarrow> set) \<Rightarrow> set \<Rightarrow> set\<close> where
+  natrec_0 [simp]: "natrec x\<^sub>0 f 0 = x\<^sub>0" and
+  natrec_succ [simp]: "natrec x\<^sub>0 f (succ n) = f (natrec x\<^sub>0 f n)"
 
-proof
-
-text \<open>
-  First construct partial functions that satisfy the recursion rule on their
-  domain of definition.
-\<close>
-
-let ?P = "\<lambda>n F.
-  F \<in> {0, ..., succ n} \<rightarrow> X \<and>
-  F `0 = x\<^sub>0 \<and>
-  (\<forall>m. m < n \<longrightarrow> F `(succ m) = f (F `m)) \<and>
-  F `(succ n) = f (F `n)"
-
-have partial_existence: "\<And>n. n: Nat \<Longrightarrow> \<exists>F. ?P n F"
-  proof -
-    fix n show "n: Nat \<Longrightarrow> \<exists>F. ?P n F"
-    proof (induction n rule: Nat_induct)
-      case base
-      let ?F = "{\<langle>0, x\<^sub>0\<rangle>, \<langle>succ 0, f x\<^sub>0\<rangle>}"
-      have "{0, ..., succ 0} = {succ 0} \<union> {0}"
-        by (simp only: upto_eq_succ) (auto intro: equalityI simp: succ_def)
-      hence "?F \<in> {0, ..., succ 0} \<rightarrow> X"
-        by (force intro: cons_FunctionI' assms)
-      thus
-        "\<exists>F. F \<in> {0, ..., succ 0} \<rightarrow> X \<and> F `0 = x\<^sub>0 \<and>
-        (\<forall>m. m < 0 \<longrightarrow> F `(succ m) = f (F `m)) \<and> F `(succ 0) = f (F `0)"
-        by auto
-
-      case (induct n)
-      obtain F where IH:
-        "F \<in> {0, ..., succ n} \<rightarrow> X"
-        "F `0 = x\<^sub>0"
-        "\<forall>m. m < n \<longrightarrow> F `(succ m) = f (F `m)"
-        "F `(succ n) = f (F `n)"
-        using induct.IH by auto
-
-      let ?F = "cons \<langle>succ (succ n), f (F `(succ n))\<rangle> F"
-
-      have 1: "?F \<in> {0, ..., succ (succ n)} \<rightarrow> X"
-        by (fastforce intro: IH simp: [[type_derivation_depth=4]])
-        \<comment>\<open>*EXAMPLE*: needs derivation depth 4\<close>
-
-      have 2: "?F `0 = x\<^sub>0"
-        using IH(2) by auto
-
-      have 3: "\<forall>m. m < succ n \<longrightarrow> ?F `(succ m) = f (?F `m)"
-        using IH by (auto simp: lt_def)
-
-      have 4: "?F `(succ (succ n)) = f (?F `(succ n))"
-        by (simp add: IH(1) mem_irrefl)
-
-      show "\<exists>F.
-        F \<in> {0, ..., succ (succ n)} \<rightarrow> X \<and>
-        F `0 = x\<^sub>0 \<and>
-        (\<forall>m. m < succ n \<longrightarrow> F `(succ m) = f (F `m)) \<and>
-        F `(succ (succ n)) = f (F `(succ n))"
-        using 1 2 3 4 by blast
-    qed
-  qed
-
-text \<open>Next show that these partial functions are unique for each n.\<close>
-
-have partial_uniqueness: "\<And>n F G. n: Nat \<Longrightarrow> ?P n F \<Longrightarrow> ?P n G \<Longrightarrow> F = G"
-  proof -
-    fix n assume "n: Nat"
-    hence succ_n: "succ n: Nat" by auto
-
-    fix F G assume F: "?P n F" and G: "?P n G" show "F = G"
-    proof (rule funext)
-      show "F \<in> {0, ..., succ n} \<rightarrow> X" and "G \<in> {0, ..., succ n} \<rightarrow> X"
-        using F G by fast+
-
-      fix m show "m \<in> {0, ..., succ n} \<Longrightarrow> F `m = G `m"
-      proof (simp only: upto_iff_le, elim le_induct[OF succ_n])
-        show "F `0 = G `0" using F G by auto
-      next
-        fix k have "k < succ n \<Longrightarrow> F `k = G `k \<longrightarrow> F `(succ k) = G `(succ k)"
-        proof (elim lt_succ_cases; clarify)
-          fix x assume x: "x < n" "F `x = G `x"
-          have "F `(succ x) = f (F `x)" using F by auto
-          also have "f (F `x) = f (G `x)" using x by auto
-          also have "f (G `x) = G `(succ x)" using G by auto
-          finally show "F `(succ x) = G `(succ x)" .
-
-          next show "F `n = G `n \<Longrightarrow> F `(succ n) = G `(succ n)"
-            using F G by auto
-        qed
-        thus "k < succ n \<Longrightarrow> F `k = G `k \<Longrightarrow> F `(succ k) = G `(succ k)" ..
-      qed
-    qed
-  qed
-
-with partial_existence have partial_functions:
-  "\<And>n. n: Nat \<Longrightarrow> \<exists>!F. ?P n F"
-  by blast
-
-text \<open>
-  Thus obtain a sequence of partial functions on \<nat> satisfying the recursion
-  relation on their restricted domains.
-\<close>
-
-define F\<^sub>P where "F\<^sub>P \<equiv> \<lambda>n. THE F. ?P n F"
-
-have F\<^sub>P_props: "\<And>n. n \<in> \<nat> \<Longrightarrow> ?P n (F\<^sub>P n)"
-  unfolding F\<^sub>P_def using partial_functions by (rule theI') auto
-
-text \<open>
-  Any two partial function are compatible on the intersection of their domains.
-\<close>
-
-have compatibility: "\<And>m n. n: Nat \<Longrightarrow> m < n \<Longrightarrow> F\<^sub>P m \<subseteq> F\<^sub>P n"
-  proof (rule extend_function)
-    fix m n
-    assume
-      n: "n: Nat" and m_le_n: "m < n"
-    hence
-      m: "m: Nat"
-      ..
-
-    from m n have Fm: "?P m (F\<^sub>P m)" and Fn: "?P n (F\<^sub>P n)"
-      unfolding F\<^sub>P_def by (fast intro!: theI' partial_functions)+
-    thus
-      "F\<^sub>P m \<in> {0, ..., succ m} \<rightarrow> X" and
-      "F\<^sub>P n \<in> {0, ..., succ n} \<rightarrow> X"
-      by auto
-
-    show "{0, ..., succ m} \<subseteq> {0, ..., succ n}"
-      unfolding upto_def by (force intro: le_lt_le m_le_n)
-
-    have Fn_restricted: "?P m (F\<^sub>P n \<restriction> {0, ..., succ m})"
-      by (auto simp:
-        restriction_function_subset Fn
-        uptoI m_le_n
-        succ_lt_le_monotone lt_transitive lt_lt_le)
-
-    text \<open>The above shows that \<open>F\<^sub>P n\<close> restricted to {0, ..., succ m} is \<open>F\<^sub>P n\<close>.\<close>
-    note partial_uniqueness[OF m Fn_restricted Fm]
-    hence
-      "\<And>k. F\<^sub>P m `k = (F\<^sub>P n \<restriction> {0, ..., succ m}) `k"
-      by auto
-    also have
-      "\<And>k. k \<in> {0, ..., succ m} \<Longrightarrow> (F\<^sub>P n \<restriction> {0, ..., succ m}) `k = F\<^sub>P n `k"
-      by auto
-    finally show
-      "\<And>k. k \<in> {0, ..., succ m} \<Longrightarrow> F\<^sub>P m `k = F\<^sub>P n `k" .
-  qed
-
-text \<open>
-  This is enough to let us construct the final recursive function and prove it
-  has the necessary properties.
-\<close>
-
-let ?F\<^sub>S = "glue {{F\<^sub>P k} | k \<in> \<nat>}"
-define F where "F \<equiv> \<lambda>n. ?F\<^sub>S `n"
-
-have F\<^sub>S_elems:
-  "\<And>f. f \<in> ?F\<^sub>S \<Longrightarrow> \<exists>n: Nat. f = F\<^sub>P n"
-  unfolding glue_def by auto
-
-have F\<^sub>S_compatible:
-  "(\<And>f g x. f \<in> ?F\<^sub>S \<Longrightarrow> g \<in> ?F\<^sub>S \<Longrightarrow> x \<in> dom f \<inter> dom g \<Longrightarrow> f `x = g `x)"
-proof -
-  fix f g
-  assume "f \<in> ?F\<^sub>S" and "g \<in> ?F\<^sub>S"
-  then obtain m n where
-    m: "m: Nat" and n: "n: Nat" and "f = F\<^sub>P m" "g = F\<^sub>P n"
-    using F\<^sub>S_elems by blast
-  then have *:
-    "dom f = {0, ..., succ m}" "dom g = {0, ..., succ n}"
-    using F\<^sub>P_props by unfold_types fast+
-
-  fix x assume "x \<in> dom f \<inter> dom g"
-  then have "x \<in> {0, ..., succ m} \<inter> {0, ..., succ n}" using * by simp
-  hence "F\<^sub>P m `x = F\<^sub>P n `x"
-
-(* show "F: Nat \<Rightarrow> element X"
-unfolding F_def
-proof unfold_types
-  have "?F\<^sub>S \<in> \<nat> \<rightarrow> X" sorry
-  thus "\<And>n. n \<in> \<nat> \<Longrightarrow> ?F\<^sub>S `n \<in> X" ..
+lemma natrec_type [type]:
+  "natrec : X \<Rightarrow> (X \<Rightarrow> X) \<Rightarrow> Nat \<Rightarrow> X"
+proof (intro typeI)
+  fix x f n
+  show "n: Nat \<Longrightarrow> x: X \<Longrightarrow> f: X \<Rightarrow> X \<Longrightarrow> natrec x f n: X"
+  by (induct n rule: Nat_induct) auto
 qed
-
-show "F 0 = x\<^sub>0"
-unfolding F_def
-proof -
-  thm apply_glue
-  have "F\<^sub>P 0 \<in> ?F\<^sub>S" unfolding glue_def by force *)
-
-oops
-
-
-section \<open>Primitive recursion\<close>
-
-definition "nat_case \<equiv> \<lambda>n\<^sub>0\<in> \<nat>. \<lambda>f\<in> \<nat> \<rightarrow> \<nat>. \<lambda>n\<in> \<nat>.
-  if n = 0 then n\<^sub>0 else f `(pred n)"
-
-text \<open>
-  Lift set-theoretic notion to HOL by reflecting the argument HOL function f to
-  the set theory.
-
-  Maybe this can be automated with syntax phase translations in the future?
-\<close>
-
-definition "natcase n\<^sub>0 f n = nat_case `n\<^sub>0 `(\<lambda>n\<in> \<nat>. f n) `n"
-
-lemma natcase_type [type]:
-  "natcase: Nat \<Rightarrow> (Nat \<Rightarrow> Nat) \<Rightarrow> Nat \<Rightarrow> Nat"
-  unfolding natcase_def nat_case_def
-  apply unfold_types
-  text \<open>Reasoning with set-theoretic function is still quite manual\<close>
-  apply (rule FunctionE)+
-  apply (rule FunctionI)+
-  by auto
-
-lemma natcase_0 [simp]:
-  assumes "n\<^sub>0 : Nat"
-      and "f : Nat \<Rightarrow> Nat"
-  shows "natcase n\<^sub>0 f 0 = n\<^sub>0"
-  unfolding natcase_def nat_case_def
-  using assms by unfold_types fastforce
-
-lemma natcase_succ [simp]:
-  assumes "n\<^sub>0 : Nat"
-      and "f : Nat \<Rightarrow> Nat"
-      and "n : Nat"
-  shows "natcase n\<^sub>0 f (succ n) = f n"
-  unfolding natcase_def nat_case_def
-  using assms by unfold_types fastforce
-
-(*THIS DEFINITION IS WRONG: it needs the well-founded recursion operator*)
-definition "natrec n\<^sub>0 f n = natcase n\<^sub>0 (\<lambda>x. f (natcase n\<^sub>0 f x)) n"
-
-text \<open>
-  The next proof is a nontrivial *EXAMPLE* of where type derivation is doing
-  some nontrivial simplification of a proof! :) :) :)
-  (The derivation depth needs to be increased in order to derive the required
-  type of the inner function \<open>\<lambda>x. f (natcase n\<^sub>0 f (pred x))\<close>.
-\<close>
-
-lemma natrec_0 [simp]:
-  assumes "n\<^sub>0 : Nat"
-      and "f : Nat \<Rightarrow> Nat"
-  shows "natrec n\<^sub>0 f 0 = n\<^sub>0"
-  unfolding natrec_def
-  using assms [[type_derivation_depth=3]] by auto
-
-lemma natrec_succ:
-  assumes "n\<^sub>0 : Nat"
-      and "f : Nat \<Rightarrow> Nat"
-      and "n : Nat"
-  shows "natrec n\<^sub>0 f (succ n) = f (natrec n\<^sub>0 f n)"
-oops
 
 
 section \<open>Addition\<close>
 
-(*
-  We might consider implementing set-theoretic lambdas for set-sized soft types
-  if we want to conform to usual type-theoretic notation. (Do we?)
-  This ventures a little into Mizar-like "sethood" territory...
-*)
+definition "nat_add m n = natrec m succ n"
 
-text \<open>This definition is rather low-level for now:\<close>
+lemma nat_add_type [type]: "nat_add : Nat \<Rightarrow> Nat \<Rightarrow> Nat"
+  unfolding nat_add_def by auto
 
-(* definition "natural_add = \<lambda>m n\<in> \<nat>. if m = 0 then n else succ *)
+no_notation plus_implicit (infixl "+" 65)
+notation nat_add (infixl "+" 65)
+
+lemma nat_add_zero_left:
+  "m: Nat \<Longrightarrow> 0 + m = m"
+  unfolding nat_add_def
+  by (induction m rule: Nat_induct) auto
+
+lemma nat_add_zero_right:
+  "m: Nat \<Longrightarrow> m + 0 = m"
+  unfolding nat_add_def
+  by (induction m rule: Nat_induct) auto
+
+lemma nat_add_assoc:
+  "k: Nat \<Longrightarrow> n: Nat \<Longrightarrow> m: Nat \<Longrightarrow> m + n + k = m + (n + k)"
+unfolding nat_add_def
+apply (induct k rule: Nat_induct)
+  apply simp
+  subgoal by (rotate_tac, rotate_tac, induct n rule: Nat_induct) auto
+done
 
 
-section \<open>Monoid properties\<close>
+section \<open>Monoid structure of (\<nat>, +)\<close>
 
+definition "Nat_monoid \<equiv> object {\<langle>@zero, 0\<rangle>, \<langle>@plus, \<lambda>m n\<in> \<nat>. nat_add m n\<rangle>}"
 
+lemma "Nat_monoid: Monoid \<nat>"
+proof unfold_types
+  show "Nat_monoid @@ zero \<in> \<nat>"
+   and "Nat_monoid @@ plus \<in> \<nat> \<rightarrow> \<nat> \<rightarrow> \<nat>"
+  unfolding Nat_monoid_def by auto
+
+  fix x assume "x \<in> \<nat>"
+  show "plus Nat_monoid (zero Nat_monoid) x = x"
+   and "plus Nat_monoid x (zero Nat_monoid) = x"
+  \<comment> \<open>Very low-level; lots of unfolding.\<close>
+  unfolding Nat_monoid_def plus_def zero_def
+  using nat_add_zero_left nat_add_zero_right
+  by auto
+
+  fix y z assume "y \<in> \<nat>" "z \<in> \<nat>"
+  show
+    "plus Nat_monoid (plus Nat_monoid x y) z =
+      plus Nat_monoid x (plus Nat_monoid y z)"
+  unfolding Nat_monoid_def plus_def zero_def
+  using nat_add_assoc by auto
+qed
 
 
 end
