8bc51d5944c1b94fe42cbd90c5e3fd928cbb1b75
Josh Chen <josh@joshchen.io>
2020-03-03 10:21:01 +0100

    prep to work on Integers

--- Isabelle_Set/Integer.thy
+++ Isabelle_Set/Integer.thy
@@ -1,6 +1,10 @@
+chapter \<open>Integers\<close>
+
 theory Integer
   imports Nat Sum Set_Extension
 begin
+
+section \<open>Construction of the carrier\<close>
 
 text \<open>
   We construct the integers as a pair of a non-negative and a negative part.
@@ -9,7 +13,7 @@ text \<open>
 
 definition "raw_int = Sum \<nat> (\<nat> \<setminus> {})"
 
-interpretation INT: set_extension \<nat> raw_int inl
+interpretation Int: set_extension \<nat> raw_int inl
 proof
   txt \<open>We must provide an injective function from \<open>\<nat>\<close> to \<open>raw_int\<close>:\<close>
 
@@ -19,14 +23,20 @@ proof
   show "\<forall>x \<in> \<nat>. \<forall>y \<in> \<nat>. inl x = inl y \<longrightarrow> x = y" by auto
 qed
 
-notation INT.def ("\<int>")
+definition "int = Int.def"
+notation int ("\<int>")
 
-lemma nat_in_int: "\<nat> \<subseteq> \<int>" by (rule INT.extension_subset)
+lemma nat_in_int: "\<nat> \<subseteq> \<int>"
+  unfolding int_def by (rule Int.extension_subset)
 
 corollary [derive]: "n : element \<nat> \<Longrightarrow> n : element \<int>"
   apply unfold_types
   apply (rule subsetE)
   by (rule nat_in_int)
+
+
+section \<open>Basic arithmetic operations\<close>
+
 
 
 end
