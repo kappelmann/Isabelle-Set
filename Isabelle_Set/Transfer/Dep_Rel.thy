3c48ffface427cb2bb0afc3a568397fc5ad38fd0
Juli Gottfriedsen <j.gottfriedsen@tum.de>
2022-03-09 09:55:34 +0100

    fix syntax

--- Isabelle_Set/Transfer/Dep_Rel.thy
+++ Isabelle_Set/Transfer/Dep_Rel.thy
@@ -2,6 +2,7 @@ theory Dep_Rel
   imports
     HOL.HOL
     HOL.Transfer
+HOL.Sledgehammer
 begin
 
 definition Rel_Fun' :: "('a \<Rightarrow> 'b \<Rightarrow> bool) \<Rightarrow> ('a \<Rightarrow> 'b \<Rightarrow> 'c \<Rightarrow> 'd \<Rightarrow> bool) \<Rightarrow>
@@ -30,6 +31,9 @@ definition no_dep_rel_weak_fun ::
   "bool \<Rightarrow> ('a \<Rightarrow> 'b \<Rightarrow> bool) \<Rightarrow> ('c \<Rightarrow> 'd \<Rightarrow> bool) \<Rightarrow> (('a \<Rightarrow> 'c) \<Rightarrow> ('b \<Rightarrow> 'd) \<Rightarrow> bool)"
   where "no_dep_rel_weak_fun P R S \<equiv> dep_rel_weak_fun P R (\<lambda>x y. S)"
 
+definition rel_weak_adj :: "bool \<Rightarrow> ('a \<Rightarrow> 'b \<Rightarrow> bool) \<Rightarrow> ('a \<Rightarrow> 'b \<Rightarrow> bool) \<Rightarrow> ('a \<Rightarrow> 'b \<Rightarrow> bool)"
+  where "rel_weak_adj P R Q \<equiv> (\<lambda>a b. P \<longrightarrow> R a b \<and> Q a b)"
+
 lemmas no_dep_rel_weak_fun_unfold = no_dep_rel_weak_fun_def[unfolded dep_rel_weak_fun_def]
 
 syntax
@@ -47,6 +51,11 @@ syntax
     \<Rightarrow> (('a \<Rightarrow> 'c) \<Rightarrow> ('b \<Rightarrow> 'd) \<Rightarrow> bool)" ("[_ \<longrightarrow> _/ _/ \<Colon>/ _] \<Rrightarrow> (_)" [101, 101, 101, 101, 100] 100)
   "_no_dep_rel_weak_fun" :: "bool \<Rightarrow> ('a \<Rightarrow> 'b \<Rightarrow> bool) \<Rightarrow> ('c \<Rightarrow> 'd \<Rightarrow> bool) \<Rightarrow>
     (('a \<Rightarrow> 'c) \<Rightarrow> ('b \<Rightarrow> 'd) \<Rightarrow> bool)" ("[_ \<longrightarrow> _] \<Rrightarrow> (_)" [101, 101, 100] 100)
+  "_rel_weak_adj" :: "bool \<Rightarrow> pttrn \<Rightarrow> pttrn \<Rightarrow> ('a \<Rightarrow> 'b \<Rightarrow> bool) \<Rightarrow> bool \<Rightarrow> ('a \<Rightarrow> 'b \<Rightarrow> bool)"
+    ("[_ \<longrightarrow> _/ _/ \<Colon>/ _/| _]" [101, 101, 101, 101, 101] 100)
+  "_dep_rel_weak_adj_fun" :: "bool \<Rightarrow> pttrn \<Rightarrow> pttrn \<Rightarrow> ('a \<Rightarrow> 'b \<Rightarrow> bool) \<Rightarrow> bool
+    \<Rightarrow> ('c \<Rightarrow> 'd \<Rightarrow> bool) \<Rightarrow> (('a \<Rightarrow> 'c) \<Rightarrow> ('b \<Rightarrow> 'd) \<Rightarrow> bool)"
+    ("[_ \<longrightarrow> _/ _/ \<Colon>/ _/| _] \<Rrightarrow> (_)" [101, 101, 101, 101, 101, 100] 100)
 
 translations
   "[x y \<Colon> R | P]" \<rightleftharpoons> "CONST rel_adj R (\<lambda>x y. P)"
@@ -54,8 +63,10 @@ translations
   "[x y \<Colon> R] \<Rrightarrow> S" \<rightleftharpoons> "CONST dep_rel_fun R (\<lambda>x y. S)"
   "[x y \<Colon> R | P] \<Rrightarrow> S" \<rightharpoonup> "CONST dep_rel_fun (CONST rel_adj R (\<lambda>x y. P)) (\<lambda>x y. S)"
   "[P \<longrightarrow> R]" \<rightleftharpoons> "CONST rel_weak P R"
-  "[P \<longrightarrow> x y \<Colon> R] \<Rrightarrow> S" \<rightleftharpoons> "CONST dep_rel_weak_fun P R (\<lambda>x y. S)"
   "[P \<longrightarrow> R] \<Rrightarrow> S" \<rightleftharpoons> "CONST no_dep_rel_weak_fun P R S"
+  "[P \<longrightarrow> x y \<Colon> R] \<Rrightarrow> S" \<rightleftharpoons> "CONST dep_rel_weak_fun P R (\<lambda>x y. S)"
+  "[P \<longrightarrow> x y \<Colon> R | Q]" \<rightleftharpoons> "CONST rel_weak_adj P R (\<lambda>x y. Q)"
+  "[P \<longrightarrow> x y \<Colon> R | Q] \<Rrightarrow> S" \<rightharpoonup> "CONST dep_rel_fun (CONST rel_weak_adj P R (\<lambda>x y. Q)) (\<lambda>x y. S)"
 
 (* Tests *)
 term "[x x' \<Colon> R | p x]"
@@ -73,5 +84,9 @@ term "[x x' \<Colon> R] \<Rrightarrow> [p x \<longrightarrow> S]"
 term "[x x' \<Colon> R] \<Rrightarrow> [p x \<longrightarrow> S] \<Rrightarrow> T"
 term "[x x' \<Colon> R] \<Rrightarrow> [p x \<longrightarrow> y y' \<Colon> S] \<Rrightarrow> T"
 term "[x x' \<Colon> R] \<Rrightarrow> [p x \<longrightarrow> y y' \<Colon> S] \<Rrightarrow> [q x y \<longrightarrow> T] \<Rrightarrow> U"
+term "[p \<longrightarrow> x x' \<Colon> R | q x]"
+term "[p \<longrightarrow> x x' \<Colon> R | q x] \<Rrightarrow> T"
+term "[p \<longrightarrow> x x' \<Colon> R | q x] \<Rrightarrow> [y y' \<Colon> T | r x y]"
+
 
 end
