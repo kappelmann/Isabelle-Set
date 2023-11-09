ac63565b36063aff33ac80975475f871e74dc606
Ri <linghanfang@outlook.com>
2023-11-09 11:34:08 +0100

    my submission

--- HOTG/Arithmetics/Arithmetics.thy
+++ HOTG/Arithmetics/Arithmetics.thy
@@ -133,35 +133,131 @@ unbundle hotg_lt_syntax
 lemma lt_iff_mem_trans_closure: "X < Y \<longleftrightarrow> X \<in> trans_closure Y"
   unfolding lt_def by simp
 
-(*this is an important lemma*)
-lemma not_subset_if_lt: "X < Y \<Longrightarrow> \<not>(Y \<subseteq> X)"
-  sorry
-
 lemma add_mem_add_if_mem:
   assumes "y \<in> z"
-  shows "(x + y) \<in> (x + z)"
+  shows "x + y \<in> x + z"
 proof-
-  have "x + z = x \<union> lift x z " by (simp add:add_eq_bin_union_lift)
-  also have "lift x z = image ((+)x) z "
+  have "lift x z = image ((+)x) z "
     by (auto simp:lift_eq_image_add)
   also have "... = {x + zz| zz \<in> z}" by simp
-  also have "y \<in> z \<Longrightarrow> x + y \<in> {x + zz| zz \<in> z}" by auto
-  then show ?thesis sorry
+  finally have lift_image:"lift x z =  {x + zz| zz \<in> z}" .
+  have add_lift:"x + z = x \<union> lift x z " 
+    by (simp add: add_eq_bin_union_lift)
+  then have add_image:"x + z = x \<union> {x + zz| zz \<in> z}"
+    by (auto simp: lift_image)
+  also have "{x + zz| zz \<in> z} \<subseteq> x \<union> {x + zz| zz \<in> z}" by auto
+  then have "{x + zz| zz \<in> z} \<subseteq> x + z" 
+    by (simp add: add_image)
+  have "y \<in> z \<Longrightarrow> x + y \<in> {x + zz| zz \<in> z}" by auto
+  then have "x + y \<in> x + z" 
+    by (auto simp: add_image assms)
+  then show ?thesis .
 qed
 
-lemma less_sub_less: "y \<in> Y \<Longrightarrow> (Y \<subseteq> X \<Longrightarrow> y \<subseteq> X) "
+(*
+lemma not_subset_if_lt: "X < Y \<Longrightarrow> \<not>(Y \<subseteq> X)"
+  sorry
+*)
+
+lemma less_sub_less: 
+  assumes "y \<in> Y" "Y < X"
+  shows "y < X"
   sorry
 
-lemma less_sub_less_conposi: "y \<in> Y \<Longrightarrow> (\<not>(y \<subseteq> X) \<Longrightarrow> \<not>(Y \<subseteq> X)) "
-  sorry
-
-lemma add_more_not_less: "\<not>((X + Y) \<subseteq> X) "
-proof (induction Y rule: mem_induction)
+lemma add_more_not_less: "\<not> X + Y < X"
+proof (induction Y arbitrary: X rule: mem_induction)
   case (mem Y)
-  have "\<And>y. y \<in> Y \<Longrightarrow> \<not>(X + y \<subseteq> X)" by (simp add: mem)
-  moreover have "\<And>y. y \<in> Y \<Longrightarrow> X + y \<in> X + Y" by (simp add: add_mem_add_if_mem)
-  ultimately show ?case sorry
-(*contradiction needed*)
+  have "\<And>y. y \<in> Y \<Longrightarrow> \<not>(X + y < X)" by (simp add: mem)
+  have step:"\<And>y. y \<in> Y \<Longrightarrow> X + y \<in> X + Y" 
+    by (simp add: add_mem_add_if_mem)
+  have "\<not> X + Y < X"
+  proof(rule ccontr)
+    assume "\<not>\<not>X + Y < X"
+    then have "X + Y < X" by simp
+    then have "\<And>x. x  \<in> X + Y \<Longrightarrow> x < X" 
+      by(auto simp: less_sub_less)
+    then have "\<And>y . y \<in> Y  \<Longrightarrow> X + y < X"
+      by (auto simp: step)
+    then show False sorry
+  qed
+  then show ?case .
+qed
+
+lemma trans_closure_inter_lift_eq_zero :"trans_closure x \<inter> lift x y = {}"
+  sorry
+
+lemma assoc_lift: "lift X (lift Y Z) = lift (X + Y) Z" 
+proof-
+  have "lift X (lift Y Z) = {X + (Y + z) | z \<in> Z}"
+    by (simp add: lift_eq_image_add)
+  also have "... =  {(X + Y) + z | z \<in> Z}"
+    by (simp add: assoc_add)
+  also have "... = lift (X + Y) Z"
+    by (simp add:lift_eq_image_add)
+  finally have "lift X (lift Y Z) = lift (X + Y) Z" .
+  then show ?thesis by simp
+qed
+
+find_theorems name:"eli"
+paragraph\<open>Proposition 3.4\<close>
+
+lemma elim_union: "A \<union> B = A \<union> C \<Longrightarrow> B = C"
+proof(induction A rule: mem_induction)
+  case (mem A)
+  then show ?case sorry
+qed
+
+lemma lift_eq_iff_eq: "lift X Y = lift X Z \<longleftrightarrow> Y = Z"
+proof (rule iffI)
+  assume "lift X Y = lift X Z"
+  thus "Y = Z"
+  proof(induction Y rule:mem_induction)
+    case (mem X)
+    then show ?case sorry
+  qed
+next
+  assume "Y = Z"
+  thus "lift X Y = lift X Z"
+    by auto
+qed
+
+lemma add_eq_iff_eq: "X + Y = X + Z \<longleftrightarrow> Y = Z"
+proof (rule iffI)
+  assume left:"X + Y = X + Z"
+  thus "Y = Z"
+  proof-
+    have "X + Y = X \<union> lift X Y" " X + Z = X \<union> lift X Z"
+      by (auto simp: add_eq_bin_union_lift)
+    then have "X \<union> lift X Y  = X \<union> lift X Z"
+      by (simp add: left)
+    then have "lift X Y = lift X Z" by (simp add: elim_union)
+    then have "Y = Z" by (simp add:lift_eq_iff_eq)
+    then show ?thesis .
+  qed
+next
+  assume "Y = Z"
+  thus "X + Y = X + Z"
+    by auto
+qed
+
+lemma add_mem_add_iif_mem: "X + Y \<in> X + Z \<longleftrightarrow> Y \<in> Z"
+proof (rule iffI)
+  assume left:"X + Y \<in> X + Z"
+  thus "Y \<in> Z"
+  proof-
+    have "X + Y \<in> X + Z"  by (simp add:left)
+    also have "... = X \<union> lift X Z"
+      by (auto simp: add_eq_bin_union_lift)
+    finally have "X + Y \<in> X \<union> lift X Z" .
+    have  add_more_not_less: "\<not> X + Y < X"
+      by (auto simp: add_more_not_less)
+    then have "X + Y \<in> lift X Z" sorry 
+    then show ?thesis sorry
+  qed
+next
+  assume "Y \<in> Z"
+  thus "X + Y \<in> X + Z"
+    by (simp add: add_mem_add_if_mem)
 qed
 
 paragraph\<open>Multiplication\<close>
@@ -172,6 +268,7 @@ bundle hotg_mul_syntax begin notation mul (infixl "*" 70) end
 bundle no_hotg_mul_syntax begin no_notation mul (infixl "*" 70) end
 unbundle hotg_mul_syntax
 
+(*can not use*)
 lemma mul_eq_union_repl_lift_mul: "X * Y = \<Union>{lift (X * y) X | y \<in> Y}"
   by (auto simp:mul_def transrec_eq)
 
@@ -180,15 +277,37 @@ lemma elts_multE:
   obtains u v where "mem u x" "mem v y" "z = x * v + u"
   sorry
 
-(*
-lemma mul_eq_mul_add: "X * Y = {X * y + r | (u \<in> y) \<and> r \<in> x }"
-*)
+lemma mul_zero_eq_zero: "x * {} = {}"
+  unfolding mul_def by (auto simp: transrec_eq)
 
-lemma mul_zero: "x * {} = {}"
-  sorry
-(*
-by (auto simp:  Union_Intersection.union_empty_eq  )
-*)
+lemma mul_one_same: "x * {{}} = x"
+  unfolding mul_def by (auto simp: transrec_eq)
+
+lemma mul_set_one_eq_lift: "X * {{{}}} = lift X X"
+proof-
+  have 1:"X * Y = \<Union>{lift (X * y) X | y \<in> Y }" 
+    by (simp add:mul_def transrec_eq)
+  have "X * {{{}}} = \<Union>{lift (X * y) X | y \<in> {{{}}} }" sorry
+  also have "... = \<Union>{lift (X * {{}}) X }" by auto
+  also have "... = \<Union>{lift X X}"
+    by (simp add: mul_one_same)
+  also have "... = lift X X" by auto
+  finally have "X * {{{}}} = lift X X" .
+    then show ?thesis by simp
+  qed
+
+lemma mul_two_eq_add: "X * {{{}}, {}} = X + X"
+proof-
+  have 1:"X * Y = \<Union>{lift (X * y) X | y \<in> Y }" 
+    by (simp add:mul_def transrec_eq)
+  have "X * {{{}}, {}} = \<Union>{lift (X * y) X | y \<in> {{{}}, {}} }" sorry
+  also have "... = (lift (X*{{}}) X) \<union> (lift (X*{}) X)" by auto
+  also have "... = lift X X \<union> lift {} X" by (simp add: mul_one_same mul_zero_eq_zero)
+  also have "... = X \<union> lift X X" by auto
+  also have "... = X + X" by (simp add: add_eq_bin_union_lift)
+  finally show ?thesis .
+qed
+
 
 
 end
