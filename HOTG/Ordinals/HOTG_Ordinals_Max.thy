ac61c26ff3802405e764c580d50f0cb67d267945
Kevin Kappelmann <accounts@kappelmann.me>
2024-05-29 16:41:13 +0200

    fix(Ordinals_Max) renaming

--- HOTG/Ordinals/HOTG_Ordinals_Max.thy
+++ HOTG/Ordinals/HOTG_Ordinals_Max.thy
@@ -14,12 +14,12 @@ lemma pred_max_ordinal_if_pred_if_pred: "P A \<Longrightarrow> P B \<Longrightar
   using max_ordinal_def by auto
 
 lemma le_max_ordinal_left_if_ordinal_if_ordinal: "ordinal A \<Longrightarrow> ordinal B \<Longrightarrow> A \<le> max_ordinal A B"
-  using ordinal_memE max_ordinal_def le_if_lt lt_if_mem by auto
+  using mem_eq_mem_if_ordinalE max_ordinal_def le_if_lt lt_if_mem by auto
 
 lemma max_ordinal_comm_if_ordinal_if_ordinal:
   assumes "ordinal A" "ordinal B"
   shows "max_ordinal A B = max_ordinal B A"
-  using assms ordinal_memE by (auto simp: max_ordinal_def not_mem_if_mem)
+  using assms mem_eq_mem_if_ordinalE by (auto simp: max_ordinal_def not_mem_if_mem)
 
 lemma le_max_ordinal_right_if_ordinal_if_ordinal: "ordinal A \<Longrightarrow> ordinal B \<Longrightarrow> B \<le> max_ordinal A B"
   using le_max_ordinal_left_if_ordinal_if_ordinal max_ordinal_comm_if_ordinal_if_ordinal by force
